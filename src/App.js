ab8dcd01f2ae2f7a1900af2fb96dea9e09608f5c
JohnAlvarezWipro <JohnAlvarezWipro@users.noreply.github.com>
2019-01-29 10:23:08 -0600

    Reactstrap works, and so dose node modules.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Landing from './containers/Landing/Landing';
 
 import './App.css';
 
 class App extends Component {
   render() {
-    return <h1>yo</h1>;
+    return <Landing />;
   }
 }
 
