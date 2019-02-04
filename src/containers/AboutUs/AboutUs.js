253aff4a6b12cc5a5cbf8f6e13861d4c857a06c8
John Alvarez (Strategic Engagements) <JO20019839@wipro.com>
2019-02-04 14:20:22 -0600

    Finished AboutUs.

--- src/containers/AboutUs/AboutUs.js
+++ src/containers/AboutUs/AboutUs.js
@@ -1,78 +1,122 @@
 import React, { Component } from 'react';
 import { Container, Row, Col } from 'reactstrap';
+
 import classes from './AboutUs.module.css';
 
 export default class AboutUs extends Component {
-  state = {
-    emp: [
-      {
-        img: 'Aboutteamsnip',
-        info:
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit Pellentesque egestas, turpis a feugiat dictum, augue mauris iaculis felis, eget ultrices magna nibh ut lectus.'
-      },
-      {
-        img: 'Aboutteamsnip',
-        info: 'test info'
-      },
-      {
-        img: 'Aboutteamsnip',
-        info: 'test info'
-      },
-      {
-        img: 'Aboutteamsnip',
-        info:
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit Pellentesque egestas, turpis a feugiat dictum, augue mauris iaculis felis, eget ultrices magna nibh ut lectus.'
-      },
-      {
-        img: 'Aboutteamsnip',
-        info:
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit Pellentesque egestas, turpis a feugiat dictum, augue mauris iaculis felis, eget ultrices magna nibh ut lectus.'
-      },
-      {
-        img: 'Aboutteamsnip',
-        info:
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit Pellentesque egestas, turpis a feugiat dictum, augue mauris iaculis felis, eget ultrices magna nibh ut lectus.'
-      },
-      {
-        img: 'Aboutteamsnip',
-        info:
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit Pellentesque egestas, turpis a feugiat dictum, augue mauris iaculis felis, eget ultrices magna nibh ut lectus.'
-      },
-      {
-        img: 'Aboutteamsnip',
-        info:
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit Pellentesque egestas, turpis a feugiat dictum, augue mauris iaculis felis, eget ultrices magna nibh ut lectus.'
-      },
-      {
-        img: 'Aboutteamsnip',
-        info:
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit Pellentesque egestas, turpis a feugiat dictum, augue mauris iaculis felis, eget ultrices magna nibh ut lectus.'
-      },
-      {
-        img: 'Aboutteamsnip',
-        info: 'test info'
-      },
-      {
-        img: 'Aboutteamsnip',
-        info: 'test info'
-      },
-      {
-        img: 'Aboutteamsnip',
-        info:
-          ' dictum, augue mauris iaculis felis, eget ultrices magna nibh ut lectus.'
-      }
-    ]
-  };
+  constructor() {
+    super();
+    this.state = {
+      currentPage: 1,
+      membersPerPage: 8,
+      members: [
+        {
+          img: 'Aboutteamsnip',
+          info:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit Pellentesque egestas, turpis a feugiat dictum, augue mauris iaculis felis, eget ultrices magna nibh ut lectus.'
+        },
+        {
+          img: 'Aboutteamsnip',
+          info: 'test info'
+        },
+        {
+          img: 'Aboutteamsnip',
+          info: 'test info'
+        },
+        {
+          img: 'Aboutteamsnip',
+          info:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit Pellentesque egestas, turpis a feugiat dictum, augue mauris iaculis felis, eget ultrices magna nibh ut lectus.'
+        },
+        {
+          img: 'Aboutteamsnip',
+          info:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit Pellentesque egestas, turpis a feugiat dictum, augue mauris iaculis felis, eget ultrices magna nibh ut lectus.'
+        },
+        {
+          img: 'Aboutteamsnip',
+          info:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit Pellentesque egestas, turpis a feugiat dictum, augue mauris iaculis felis, eget ultrices magna nibh ut lectus.'
+        },
+        {
+          img: 'Aboutteamsnip',
+          info:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit Pellentesque egestas, turpis a feugiat dictum, augue mauris iaculis felis, eget ultrices magna nibh ut lectus.'
+        },
+        {
+          img: 'Aboutteamsnip',
+          info:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit Pellentesque egestas, turpis a feugiat dictum, augue mauris iaculis felis, eget ultrices magna nibh ut lectus.'
+        },
+        {
+          img: 'Aboutteamsnip',
+          info:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit Pellentesque egestas, turpis a feugiat dictum, augue mauris iaculis felis, eget ultrices magna nibh ut lectus.'
+        },
+        {
+          img: 'Aboutteamsnip',
+          info: 'test info'
+        },
+        {
+          img: 'Aboutteamsnip',
+          info: 'test info'
+        },
+        {
+          img: 'Aboutteamsnip',
+          info:
+            ' dictum, augue mauris iaculis felis, eget ultrices magna nibh ut lectus.'
+        }
+      ]
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
 
+  handleClick(event) {
+    this.setState({
+      currentPage: Number(event.target.id)
+    });
+  }
   render() {
-    const elements = this.state.emp.map((ele, i) => (
-      <Col key={i} md="3">
-        <div className={classes.Member}>
-          <img alt={ele.img} src={require('../../assets/img/images.png')} />
-          <p>{ele.info}</p>
-        </div>
-      </Col>
-    ));
+    const { members, currentPage, membersPerPage } = this.state;
+    const indexOfLastMember = currentPage * membersPerPage;
+    const indexOfFirstMember = indexOfLastMember - membersPerPage;
+    const currentMembers = members.slice(indexOfFirstMember, indexOfLastMember);
+
+    const renderMembers = currentMembers.map((member, index) => {
+      return (
+        <Col key={index} md="3">
+          <div className={classes.Member}>
+            <img
+              alt={member.img}
+              src={require('../../assets/img/images.png')}
+            />
+            <p>{member.info}</p>
+          </div>
+        </Col>
+      );
+    });
+
+    const pageNumbers = [];
+    for (let i = 1; i <= Math.ceil(members.length / membersPerPage); i++) {
+      pageNumbers.push(i);
+    }
+
+    const renderPageNumbers = pageNumbers.map(number => {
+      const isActive =
+        number === this.state.currentPage
+          ? classes.PageNumberActive
+          : classes.PageNumberNotActive;
+      return (
+        <li
+          key={number}
+          id={number}
+          className={isActive}
+          onClick={this.handleClick}
+        >
+          &bull;
+        </li>
+      );
+    });
 
     return (
       <div className={classes.AboutUs}>
@@ -80,7 +124,8 @@ export default class AboutUs extends Component {
           <h2 className={classes.Header}>About Our Team</h2>
         </div>
         <Container>
-          <Row>{elements}</Row>
+          <Row>{renderMembers}</Row>
+          <div className={classes.Pages}>{renderPageNumbers}</div>
         </Container>
       </div>
     );
