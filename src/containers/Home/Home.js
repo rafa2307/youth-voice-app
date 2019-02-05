9d3f1d9d5f014199748a5a8a7e24759443820ce8
JohnAlvarezWipro <JohnAlvarezWipro@users.noreply.github.com>
2019-02-05 14:05:19 -0600

    Rendered Programs with map.

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -5,7 +5,39 @@ import AppCarousel from '../AppCarousel/AppCarousel';
 import classes from './Home.module.css';
 
 export default class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      programs: [
+        {
+          img: require('../../assets/img/YouthVoicesLogo.jpg'),
+          alt: 'Youth Voices',
+          text:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit.Pellentesque egestas, turpis a feugiat dictum, augue maurisiaculis felis, eget ultrices magna nibh ut lectus.'
+        },
+        {
+          img: require('../../assets/img/YouthVoicesLogo.jpg'),
+          alt: 'Youth Voices',
+          text:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit.Pellentesque egestas, turpis a feugiat dictum, augue maurisiaculis felis, eget ultrices magna nibh ut lectus.'
+        }
+      ]
+    };
+  }
+
   render() {
+    const { programs } = this.state;
+    const renderPrograms = programs.map((program, index) => {
+      return (
+        <Program
+          key={index}
+          ProgramImage={program.img}
+          ImageAlt={program.alt}
+          P={program.text}
+        />
+      );
+    });
+
     return (
       <div className={classes.Home}>
         <AppCarousel />
@@ -13,22 +45,7 @@ export default class Home extends Component {
           <Col>
             <h1>OUR PROGRAMS</h1>
           </Col>
-          <Row>
-            <Program
-              ProgramImage={require('../../assets/img/YouthVoicesLogo.jpg')}
-              ImageAlt="Youth Voices"
-              P="Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Pellentesque egestas, turpis a feugiat dictum, augue mauris
-              iaculis felis, eget ultrices magna nibh ut lectus."
-            />
-            <Program
-              ProgramImage={require('../../assets/img/VoicesWithoutBorders.png')}
-              ImageAlt="Voices Without Borders"
-              P="Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Pellentesque egestas, turpis a feugiat dictum, augue mauris
-              iaculis felis, eget ultrices magna nibh ut lectus."
-            />
-          </Row>
+          <Row>{renderPrograms}</Row>
         </Container>
       </div>
     );
