a725cbf9c5c9e948540c785e2ae99f4d360dafbd
JohnAlvarezWipro <JohnAlvarezWipro@users.noreply.github.com>
2019-02-11 13:46:52 -0600

    Storys now uses redux.

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -79,6 +79,164 @@ const initialState = {
       img: require('../assets/img/Aboutteamsnip.png'),
       alt: 'img'
     }
+  ],
+  stories: [
+    {
+      header: 'Story 1',
+      src: 'https://www.youtube.com/channel/UCZLV4ZUg89Sgz7Zxm_p7PRA',
+      text:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing joe. Pellentesque egestas, turpis a feugiat dictum'
+    },
+    {
+      header: 'Story 2',
+      src: 'https://www.youtube.com/channel/UCZLV4ZUg89Sgz7Zxm_p7PRA',
+      text:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing joe. Pellentesque egestas, turpis a feugiat dictum'
+    },
+    {
+      header: 'bob 3',
+      src: 'https://www.youtube.com/channel/UCZLV4ZUg89Sgz7Zxm_p7PRA',
+      text:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing joe. Pellentesque egestas, turpis a feugiat dictum'
+    },
+    {
+      header: 'Story 4',
+      src: 'https://www.youtube.com/channel/UCZLV4ZUg89Sgz7Zxm_p7PRA',
+      text:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing joe. Pellentesque egestas, turpis a feugiat dictum'
+    },
+    {
+      header: 'Story 5',
+      src: 'https://www.youtube.com/channel/UCZLV4ZUg89Sgz7Zxm_p7PRA',
+      text:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing joe. Pellentesque egestas, turpis a feugiat dictum'
+    },
+    {
+      header: 'Story 6',
+      src: 'https://www.youtube.com/channel/UCZLV4ZUg89Sgz7Zxm_p7PRA',
+      text:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing joe. Pellentesque egestas, turpis a feugiat dictum'
+    },
+    {
+      header: 'Story 7',
+      src: 'https://www.youtube.com/channel/UCZLV4ZUg89Sgz7Zxm_p7PRA',
+      text:
+        'Lorem ipsum dolor sit bob, consectetur adipiscing joe. Pellentesque egestas, turpis a feugiat dictum'
+    },
+    {
+      header: 'Story 8',
+      src: 'https://www.youtube.com/channel/UCZLV4ZUg89Sgz7Zxm_p7PRA',
+      text:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing joe. Pellentesque egestas, turpis a feugiat dictum'
+    },
+    {
+      header: 'Story 9',
+      src: 'https://www.youtube.com/channel/UCZLV4ZUg89Sgz7Zxm_p7PRA',
+      text:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing joe. Pellentesque egestas, turpis a feugiat dictum'
+    },
+    {
+      header: 'Story 10',
+      src: 'https://www.youtube.com/channel/UCZLV4ZUg89Sgz7Zxm_p7PRA',
+      text:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing joe. Pellentesque egestas, turpis a feugiat dictum'
+    },
+    {
+      header: 'Story 11',
+      src: 'https://www.youtube.com/channel/UCZLV4ZUg89Sgz7Zxm_p7PRA',
+      text:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing joe. Pellentesque egestas, turpis a feugiat dictum'
+    },
+    {
+      header: 'Story 12',
+      src: 'https://www.youtube.com/channel/UCZLV4ZUg89Sgz7Zxm_p7PRA',
+      text:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque egestas, turpis a feugiat dictum'
+    },
+    {
+      header: 'Story 13',
+      src: 'https://www.youtube.com/channel/UCZLV4ZUg89Sgz7Zxm_p7PRA',
+      text:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque egestas, turpis a feugiat dictum'
+    },
+    {
+      header: 'Story 14',
+      src: 'https://www.youtube.com/channel/UCZLV4ZUg89Sgz7Zxm_p7PRA',
+      text:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque egestas, turpis a feugiat dictum'
+    },
+    {
+      header: 'Story 15',
+      src: 'https://www.youtube.com/channel/UCZLV4ZUg89Sgz7Zxm_p7PRA',
+      text:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque egestas, turpis a feugiat dictum'
+    },
+    {
+      header: 'Story 16',
+      src: 'https://www.youtube.com/channel/UCZLV4ZUg89Sgz7Zxm_p7PRA',
+      text:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque egestas, turpis a feugiat dictum'
+    },
+    {
+      header: 'Story 17',
+      src: 'https://www.youtube.com/channel/UCZLV4ZUg89Sgz7Zxm_p7PRA',
+      text:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque egestas, turpis a feugiat dictum'
+    },
+    {
+      header: 'Story 18',
+      src: 'https://www.youtube.com/channel/UCZLV4ZUg89Sgz7Zxm_p7PRA',
+      text:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque egestas, turpis a feugiat dictum'
+    },
+    {
+      header: 'Story 19',
+      src: 'https://www.youtube.com/channel/UCZLV4ZUg89Sgz7Zxm_p7PRA',
+      text:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque egestas, turpis a feugiat dictum'
+    },
+    {
+      header: 'Story 20',
+      src: 'https://www.youtube.com/channel/UCZLV4ZUg89Sgz7Zxm_p7PRA',
+      text:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque egestas, turpis a feugiat dictum'
+    },
+    {
+      header: 'Story 21',
+      src: 'https://www.youtube.com/channel/UCZLV4ZUg89Sgz7Zxm_p7PRA',
+      text:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque egestas, turpis a feugiat dictum'
+    },
+    {
+      header: 'Story 22',
+      src: 'https://www.youtube.com/channel/UCZLV4ZUg89Sgz7Zxm_p7PRA',
+      text:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque egestas, turpis a feugiat dictum'
+    },
+    {
+      header: 'Story 23',
+      src: 'https://www.youtube.com/channel/UCZLV4ZUg89Sgz7Zxm_p7PRA',
+      text:
+        'Lorem ipsum dolor sit amet, bob adipiscing elit. Pellentesque egestas, turpis a feugiat dictum'
+    },
+    {
+      header: 'Story 24',
+      src: 'https://www.youtube.com/channel/UCZLV4ZUg89Sgz7Zxm_p7PRA',
+      text:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque egestas, turpis a feugiat dictum'
+    },
+    {
+      header: 'Story 25',
+      src: 'https://www.youtube.com/channel/UCZLV4ZUg89Sgz7Zxm_p7PRA',
+      text:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque egestas, turpis a feugiat dictum'
+    },
+    {
+      header: 'Story 26',
+      src: 'https://www.youtube.com/channel/UCZLV4ZUg89Sgz7Zxm_p7PRA',
+      text:
+        'Lorem ipsum dolor sit amet, story adipiscing elit. Pellentesque egestas, turpis a feugiat dictum'
+    }
   ]
 };
 
